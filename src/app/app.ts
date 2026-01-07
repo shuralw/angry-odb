29200c7951299123539350f5d62a67ec9056d88b
Lukas Wessel <wessel5991+github@gmail.com>
2026-01-07 21:39:56 +0100

    basic Raster

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angry-odb');
+  protected readonly gridSize = 9;
+  protected readonly rows = Array.from({ length: this.gridSize });
+  protected readonly cols = Array.from({ length: this.gridSize });
+  protected readonly imageSrc = '/odb_default_v5.png';
 }
